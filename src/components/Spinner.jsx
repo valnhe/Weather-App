c2fb091279e298d848e2f1b6cc42885ee067b584
valnhe <vpazcastle@gmail.com>
2024-01-16 13:53:11 -0300

    Add Spinner component and its CSS

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,8 @@
+import "./Spinner.css";
+
+function Spinner () {
+    return (
+        <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
+} export default Spinner;
+
